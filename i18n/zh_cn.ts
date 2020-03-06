713e372fba19c000e3c9b44f93e8668f8b8efda0
Gloomy Ghost <740291272@qq.com>
2020-03-06 15:00:50 +0800

    add i18n file and translate to Chinese

---
+++ i18n/zh_cn.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="23"/>
+        <source>Raspberry Pi Imager</source>
+        <translation>树莓派镜像烧录</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="69"/>
+        <location filename="../main.qml" line="280"/>
+        <source>Operating System</source>
+        <translation>操作系统</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="81"/>
+        <source>CHOOSE OS</source>
+        <translation>选择操作系统</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="104"/>
+        <location filename="../main.qml" line="578"/>
+        <source>SD Card</source>
+        <translation>SD卡</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="116"/>
+        <location filename="../main.qml" line="892"/>
+        <source>CHOOSE SD CARD</source>
+        <translation>选择SD卡</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="144"/>
+        <source>WRITE</source>
+        <translation>烧录</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="159"/>
+        <location filename="../main.qml" line="840"/>
+        <source>Writing... %1%</source>
+        <translation>写入中...%1%</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="197"/>
+        <source>CANCEL WRITE</source>
+        <translation>取消写入</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="200"/>
+        <location filename="../main.qml" line="837"/>
+        <source>Cancelling...</source>
+        <translation>取消中...</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="211"/>
+        <source>CANCEL VERIFY</source>
+        <translation>取消验证</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="214"/>
+        <location filename="../main.qml" line="860"/>
+        <location filename="../main.qml" line="910"/>
+        <source>Finalizing...</source>
+        <translation>完成中...</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="342"/>
+        <source>Erase</source>
+        <translation>擦除</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="343"/>
+        <source>Format card as FAT32</source>
+        <translation>格式化SD卡为FAT32格式</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="349"/>
+        <source>Use custom</source>
+        <translation>使用自定义镜像</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="350"/>
+        <source>Select a custom .img from your computer</source>
+        <translation>使用下载的系统镜像文件烧录</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="357"/>
+        <location filename="../main.qml" line="501"/>
+        <source>Error parsing os_list.json</source>
+        <translation>解析 os_list.json 错误</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="381"/>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="382"/>
+        <source>Go back to main menu</source>
+        <translation>回到主页</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="436"/>
+        <source>Released: %1</source>
+        <translation>解压中...%1</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="439"/>
+        <source>Cached on your computer</source>
+        <translation>在你的电脑上缓存</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="441"/>
+        <source>Online - %1 GB download</source>
+        <translation>已下载：%1 GB</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="653"/>
+        <source>Mounted as %1</source>
+        <translation>挂载在：%1 上</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="769"/>
+        <source>QUIT APP</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="779"/>
+        <source>CONTINUE</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="819"/>
+        <source>Error downloading OS list from Internet</source>
+        <translation>下载镜像列表错误</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="863"/>
+        <source>Verifying... %1%</source>
+        <translation>验证文件中...%1%</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="881"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="888"/>
+        <source>Write Successful</source>
+        <translation>烧录成功</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="889"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; has been written to &lt;b&gt;%2&lt;/b&gt;&lt;br&gt;&lt;br&gt;You can now remove the SD card from the reader</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; 已经成功烧录到 &lt;b&gt;%2&lt;/b&gt;&lt;br&gt;&lt;br&gt;上了，你可以卸载SD卡了</translation>
+    </message>
+</context>
+</TS>
