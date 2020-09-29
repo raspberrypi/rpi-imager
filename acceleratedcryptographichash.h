a6a4f0fc5b628811499a9163a5a4b487041f9c36
Floris Bos <bos@je-eigen-domein.nl>
2020-09-29 19:56:12 +0200

    Mac: use system hash functions
    May perform better under emulation

--- acceleratedcryptographichash.h
+++ acceleratedcryptographichash.h
@@ -9,10 +9,15 @@
 #include <QCryptographicHash>
 
 #ifdef Q_OS_DARWIN
-typedef QCryptographicHash AcceleratedCryptographicHash;
+#include <CommonCrypto/CommonDigest.h>
+#define SHA256_CTX    CC_SHA256_CTX
+#define SHA256_DIGEST_LENGTH  CC_SHA256_DIGEST_LENGTH
+#define SHA256_Init   CC_SHA256_Init
+#define SHA256_Update CC_SHA256_Update
+#define SHA256_Final  CC_SHA256_Final
 #else
-
 #include "openssl/sha.h"
+#endif
 
 class AcceleratedCryptographicHash
 {
@@ -27,5 +32,4 @@ protected:
     SHA256_CTX _sha256;
 };
 
-#endif
 #endif // ACCELERATEDCRYPTOGRAPHICHASH_H
