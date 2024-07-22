ff3b9173c374de3534e1124ee0492cc6a6a92351
Tom Dewey <tom.dewey@raspberrypi.com>
2024-07-22 11:32:49 +0100

    libarchive: include build infra

---
+++ src/dependencies/libarchive-3.7.4/build/ci/github_actions/macos.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ "$1" = "prepare" ]
+then
+	set -x
+	brew uninstall openssl@1.0.2t > /dev/null
+	brew uninstall python@2.7.17 > /dev/null
+	brew untap local/openssl > /dev/null
+	brew untap local/python2 > /dev/null
+	brew update > /dev/null
+	brew upgrade > /dev/null
+	set -x -e
+	for pkg in \
+		autoconf \
+		automake \
+		libtool \
+		pkg-config \
+		cmake \
+		xz \
+		lz4 \
+		zstd \
+		libxml2 \
+		openssl
+	do
+		brew list $pkg > /dev/null && brew upgrade $pkg || brew install $pkg
+	done
+fi
