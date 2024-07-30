1ee9c3749a14d51493b1cc13875c867e799d37f3
Tom Dewey <tom.dewey@raspberrypi.com>
2024-07-30 15:03:11 +0100

    AcceleratedCryptographicHash: Use platform capabilities
    Introduce a CNG version for Windows, a CommonCrypto
    version for macOS and a GnuTLS version for Linux, as
    we're using gnutls _anyway_.

--- src/acceleratedcryptographichash.h
+++ src/acceleratedcryptographichash.h
@@ -7,37 +7,20 @@
  */
 
 #include <QCryptographicHash>
+#include <memory>
 
-#ifdef Q_OS_DARWIN
-#include <CommonCrypto/CommonDigest.h>
-#define SHA256_CTX    CC_SHA256_CTX
-#define SHA256_DIGEST_LENGTH  CC_SHA256_DIGEST_LENGTH
-#define SHA256_Init   CC_SHA256_Init
-#define SHA256_Update CC_SHA256_Update
-#define SHA256_Final  CC_SHA256_Final
-#else
-#ifdef HAVE_GNUTLS
-#include "gnutls/crypto.h"
-#else
-#include "openssl/sha.h"
-#endif
-#endif
-
-class AcceleratedCryptographicHash
+struct AcceleratedCryptographicHash
 {
+private:
+    struct impl;
+    std::unique_ptr<impl> p_Impl;
+
 public:
     explicit AcceleratedCryptographicHash(QCryptographicHash::Algorithm method);
-    virtual ~AcceleratedCryptographicHash();
+    ~AcceleratedCryptographicHash();
     void addData(const char *data, int length);
     void addData(const QByteArray &data);
     QByteArray result();
-
-protected:
-#ifdef HAVE_GNUTLS
-    gnutls_hash_hd_t _sha256;
-#else
-    SHA256_CTX _sha256;
-#endif
 };
 
 #endif // ACCELERATEDCRYPTOGRAPHICHASH_H
---
+++ src/windows/acceleratedcryptographichash_cng.cpp
@@ -0,0 +1,179 @@
+/*
+ * Use Cryptography API: Next Generation for SHA256
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2024 Raspberry Pi Ltd
+ */
+
+#include "acceleratedcryptographichash.h"
+
+#include <windows.h>
+#include <bcrypt.h>
+
+#define NT_SUCCESS(Status)          (((NTSTATUS)(Status)) >= 0)
+
+#define STATUS_UNSUCCESSFUL         ((NTSTATUS)0xC0000001L)
+
+struct AcceleratedCryptographicHash::Impl {
+    explicit Impl(QCryptographicHash::Algorithm algo) {
+        if (method != QCryptographicHash::Sha256)
+            throw std::runtime_error("Only sha256 implemented");
+
+        //open an algorithm handle
+        if(!NT_SUCCESS(status = BCryptOpenAlgorithmProvider(
+                                                    &hAlg,
+                                                    BCRYPT_SHA256_ALGORITHM,
+                                                    NULL,
+                                                    0)))
+        {
+            qDebug() << "BCryptOpenAlgorithmProvider returned Error " << status;
+            goto Cleanup;
+        }
+
+        //calculate the size of the buffer to hold the hash object
+        if(!NT_SUCCESS(status = BCryptGetProperty(
+                                            hAlg, 
+                                            BCRYPT_OBJECT_LENGTH, 
+                                            (PBYTE)&cbHashObject, 
+                                            sizeof(DWORD), 
+                                            &cbData, 
+                                            0)))
+        {
+            qDebug() <<  "BCryptGetProperty returned Error " << status;
+            goto Cleanup;
+        }
+
+        //allocate the hash object on the heap
+        pbHashObject = (PBYTE)HeapAlloc (GetProcessHeap(), 0, cbHashObject);
+        if(NULL == pbHashObject)
+        {
+            qDebug() <<  "memory allocation failed";
+            goto Cleanup;
+        }
+
+    //calculate the length of the hash
+        if(!NT_SUCCESS(status = BCryptGetProperty(
+                                            hAlg, 
+                                            BCRYPT_HASH_LENGTH, 
+                                            (PBYTE)&cbHash, 
+                                            sizeof(DWORD), 
+                                            &cbData, 
+                                            0)))
+        {
+            qDebug() << "BCryptGetProperty returned Error " << status;
+            goto Cleanup;
+        }
+
+        //allocate the hash buffer on the heap
+        pbHash = (PBYTE)HeapAlloc (GetProcessHeap(), 0, cbHash);
+        if(NULL == pbHash)
+        {
+            qDebug() <<  "memory allocation failed";
+            cleanup();
+            return;
+        }
+
+        //create a hash
+        if(!NT_SUCCESS(status = BCryptCreateHash(
+                                            hAlg, 
+                                            &hHash, 
+                                            pbHashObject, 
+                                            cbHashObject, 
+                                            NULL, 
+                                            0, 
+                                            0)))
+        {
+            qDebug() << "BCryptCreateHash returned Error " << status;
+            cleanup();
+            return;
+        }
+    }
+
+    ~Impl() {
+        cleanup()
+    }
+
+    void cleanup() {
+        if(hAlg)
+        {
+            BCryptCloseAlgorithmProvider(hAlg,0);
+        }
+
+        if (hHash)    
+        {
+            BCryptDestroyHash(hHash);
+        }
+
+        if(pbHashObject)
+        {
+            HeapFree(GetProcessHeap(), 0, pbHashObject);
+        }
+
+        if(pbHash)
+        {
+            HeapFree(GetProcessHeap(), 0, pbHash);
+        } 
+    }
+
+    void addData(const char *data, int length)
+    {
+        //hash some data
+        if(!NT_SUCCESS(status = BCryptHashData(
+                                            hHash,
+                                            (PBYTE)data,
+                                            length,
+                                            0)))
+        {
+            qDebug() << "BCryptHashData returned Error " << status;
+            goto Cleanup;
+        }
+    }
+
+    void addData(const QByteArray &data)
+    {
+        addData(data.constData(), data.size());
+    }
+
+    QByteArray result() {
+            //close the hash
+        if(!NT_SUCCESS(status = BCryptFinishHash(
+                                            hHash, 
+                                            pbHash, 
+                                            cbHash, 
+                                            0)))
+        {
+            qDebug() << "BCryptFinishHash returned Error " << status;
+            cleanup()
+            return {};
+        } else {
+            // No cleanup required, as the dtor of this class will do so.
+            auto returnArray = QByteArray(pbHash, cbHash);
+            return returnArray;
+        }
+    }
+
+private:
+    BCRYPT_ALG_HANDLE       hAlg            = NULL;
+    BCRYPT_HASH_HANDLE      hHash           = NULL;
+    NTSTATUS                status          = STATUS_UNSUCCESSFUL;
+    DWORD                   cbData          = 0,
+                            cbHash          = 0,
+                            cbHashObject    = 0;
+    PBYTE                   pbHashObject    = NULL;
+    PBYTE                   pbHash          = NULL;
+};
+
+AcceleratedCryptographicHash::AcceleratedCryptographicHash(QCryptographicHash::Algorithm method)
+    : p_Impl(std::make_unique<impl>(method)) {}
+
+AcceleratedCryptographicHash::~AcceleratedCryptographicHash() = default;
+
+void AcceleratedCryptographicHash::addData(const char *data, int length) {
+    p_Impl->addData(data, length);
+}
+void AcceleratedCryptographicHash::addData(const QByteArray &data) {
+    p_Impl->addData(data);
+}
+QByteArray AcceleratedCryptographicHash::result() {
+    return p_Impl->result();
+}
