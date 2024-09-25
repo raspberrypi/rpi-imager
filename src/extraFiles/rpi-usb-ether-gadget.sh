04e63788616e0e4bdc7f7d0ff102a461c74c588e
paulober <44974737+paulober@users.noreply.github.com>
2024-09-25 10:43:41 +0100

    Update ether gadget toggle script
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- src/extraFiles/rpi-usb-ether-gadget.sh
+++ src/extraFiles/rpi-usb-ether-gadget.sh
@@ -8,20 +8,19 @@ fi
 
 # check if the script is run on a Raspberry Pi
 if ! grep -q "Raspberry Pi" /proc/device-tree/model; then
-    echo -e "\e[31mThis script is for Raspberry Pi only\e[0m"
+    echo -e "\e[31mThis script is for Raspberry Pi devices only\e[0m"
     exit
 fi
 
-FORCE_ON=false
-FORCE_OFF=false
-# check if command line arguments to force on or to force off are provided if not continue with
-# check else force
+TURN_ON=true
 if [ "$1" == "on" ]; then
-    FORCE_ON=true
+    TURN_ON=true
 elif [ "$1" == "off" ]; then
-    FORCE_OFF=true
+    TURN_ON=false
 elif [ "$1" == "toggle" ]; then
-    :  # Do nothing and continue to the code below
+    if [ -f /etc/modules-load.d/usb-ether-gadget.conf ]; then
+        TURN_ON=false
+    fi
 elif [ "$1" == "status" ]; then
     if [ -f /etc/modules-load.d/usb-ether-gadget.conf ]; then
         echo -e "\e[33mUSB Ethernet Gadget is on\e[0m"
@@ -29,15 +28,14 @@ elif [ "$1" == "status" ]; then
         echo -e "\e[33mUSB Ethernet Gadget is off\e[0m"
     fi
     exit
-else # add else if to make toggling possible without toggle keyword
+else
     echo "Usage: rpi-usb-ether-gadget [on|off|toggle|status|help]"
     exit
 fi
 
 #rm /etc/modprobe.d/g_ether.conf
 
-# check if /etc/modules-load.d/usb-ether-gadget.conf exists to know if to turn of or on or the force flags are set
-if ([ -f /etc/modules-load.d/usb-ether-gadget.conf ] && [ "$FORCE_ON" = false ]) || [ "$FORCE_OFF" = true ]; then
+if [ "$TURN_ON" = false ]; then
     echo "Turning \e[31moff\e[0m USB Ethernet Gadget mode"
     rm -f /etc/modules-load.d/usb-ether-gadget.conf
     sed -i '/dtoverlay=dwc2,dr_mode=peripheral/d' /boot/firmware/config.txt
