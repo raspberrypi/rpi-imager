0846b06081285977c1d7c1e977a32a6bdd7da46f
cjhilyard <chuck.j.hilyard@gmail.com>
2025-01-12 15:02:48 -0800

    cleanup gitignore

---
+++ build.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# ensure we're running via sudo
+if [ $(whoami) = "root" ]
+then
+echo "executing as $(whoami)"
+
+else
+echo "error: must run \"sudo $0\""
+exit
+fi
+
+
+# is this a raspberry pi 5?
+if [ $(uname -r) = "6.11.0-1004-raspi" ]
+        then
+                echo "OS version match"
+        else
+                echo "OS version mismatch"
+                exit
+fi
+
+echo "****** apt update ******"
+apt-get update -y
+echo "****** apt upgrade ******"
+apt-get upgrade -y
+echo "****** apt install ******"
+apt-get install -y ntpdate sudo vim htop tar iotop bridge-utils cpu-checker mysql-server
+
+ntpdate time.nist.gov
+
+update_mysqld() {
+echo "****** update_mysqld ******"
+
+cat <<HereDoc >> /etc/mysql/mysql.conf.d/mysqld.cnf
+server_id = 1
+sql-mode="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION,ERROR_FOR_DIVISION_BY_ZERO,NO_ZERO_DATE,NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION"
+innodb_rollback_on_timeout=1
+innodb_lock_wait_timeout=600
+max_connections=1000
+log-bin=mysql-bin
+binlog-format = 'ROW'
+
+default-authentication-plugin=mysql_native_password
+HereDoc
+}
+
+if [ $(grep "ERROR_FOR_DIVISION_BY_ZERO" /etc/mysql/mysql.conf.d/mysqld.cnf) ]
+then
+        echo "mysqld.cnf already updated"
+        return
+else
+        echo "updating mysqld.cnf"
+        update_mysqld
+fi
+
+echo "****** restart mysql ******"
+systemctl restart mysql
+
+
+# Setup Repo
+echo "****** setup apt repo ******"
+mkdir -p /etc/apt/keyrings
+wget -O- http://packages.shapeblue.com/release.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/cloudstack.gpg > /dev/null
+echo deb [signed-by=/etc/apt/keyrings/cloudstack.gpg] http://packages.shapeblue.com/cloudstack/upstream/debian/4.19 / > /etc/apt/sources.list.d/cloudstack.list
+
+# Install management server
+echo "****** install cloudstack ******"
+apt-get update
+apt-get install cloudstack-management cloudstack-usage
+
+# Stop the automatic start after install
+systemctl stop cloudstack-management cloudstack-usage
+
+echo "****** setup database ******"
+cloudstack-setup-databases cloud:cloud@localhost --deploy-as=root: -i 192.168.1.10
+
+echo "****** kvm setup ******"
+apt-get install -y qemu-kvm cloudstack-agent
+systemctl stop cloudstack-agent
+sed -i -e 's/\#vnc_listen.*$/vnc_listen = "0.0.0.0"/g' /etc/libvirt/qemu.conf
+echo 'security_driver = "none"' >> /etc/libvirt/qemu.conf
+echo LIBVIRTD_ARGS=\"--listen\" >> /etc/default/libvirtd
+
+echo 'listen_tls=0' >> /etc/libvirt/libvirtd.conf
+echo 'listen_tcp=1' >> /etc/libvirt/libvirtd.conf
+echo 'tcp_port = "16509"' >> /etc/libvirt/libvirtd.conf
+echo 'mdns_adv = 0' >> /etc/libvirt/libvirtd.conf
+echo 'auth_tcp = "none"' >> /etc/libvirt/libvirtd.conf
+
+echo "****** configure iptables ******"
+echo "doing nothing"
+
+# exit sudo
+exit
+
+
+echo "****** start cloudstack ******"
+cloudstack-setup-management
+systemctl status cloudstack-management
+tail -f /var/log/cloudstack/management/management-server.log
+
