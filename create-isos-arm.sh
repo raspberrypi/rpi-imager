df14a5da45e0342740f5f908d97682644996e17f
cjhilyard <chuck.j.hilyard@gmail.com>
2025-01-26 18:25:47 -0800

    init

---
+++ create-isos-arm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/apiextensions-apiserver
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-aggregator
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-apiserver
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-controller-manager
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-log-runner
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-proxy
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-scheduler
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubeadm
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl-convert
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubelet
+# https://dl.k8s.io/v1.32.1/bin/linux/arm64/mounter
