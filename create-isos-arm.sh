aa50eec7c0c5d9f10fca191f8a21f898a5acb44d
cjhilyard <chuck.j.hilyard@gmail.com>
2025-01-27 17:05:02 -0800

    iso builder

--- create-isos-arm.sh
+++ create-isos-arm.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/apiextensions-apiserver
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-aggregator
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-apiserver
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-controller-manager
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-log-runner
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-proxy
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-scheduler
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubeadm
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl-convert
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubelet
-# https://dl.k8s.io/v1.32.1/bin/linux/arm64/mounter
+urls="
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/apiextensions-apiserver
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-aggregator
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-apiserver
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-controller-manager
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-log-runner
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-proxy
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kube-scheduler
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubeadm
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubectl-convert
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/kubelet
+https://dl.k8s.io/v1.32.1/bin/linux/arm64/mounter
+"
+
+for url in $(echo ${urls})
+do
+	echo "url: $url"
+done
