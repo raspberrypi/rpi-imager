fc0c72b7edaa4abac8454ce9efc90a3787baa2df
Tom Dewey <tom.dewey@raspberrypi.com>
2025-10-07 17:20:06 +0100

    imageadvancedoptions: Create placeholder cpp file
    This ensures moc generates the meta-object code for both CLI and GUI builds, not just when it's coincidentally included in GUI code.

---
+++ src/imageadvancedoptions.cpp
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2025 Raspberry Pi Ltd
+ */
+
+#include "imageadvancedoptions.h"
+
+// This file is required for Q_NAMESPACE to work properly
+// The moc system needs a .cpp file to generate the staticMetaObject
+
